6e19b2ef9e9b8a0d95b4726ea492e63ba93e7b61
PoojaSolankar <poojasolankar09gmail.com>
2022-02-28 00:22:44 +0530

    first java program

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package javaApp;
+
+
+class Test{
+	
+	public void demo(int a ,int b)
+	{
+	
+		 int c=a+b;
+		  System.out.println(c);
+	}
+}
+
+public class HelloWorld {
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello World"); 
+           Test o = new Test(); 
+           o.demo(2,7);
+        
+	}
+
+}
